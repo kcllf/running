e19d31948171a866d7a59a323d5cad288fa526de
kcllf <kcllf@qq.com>
2021-09-22 01:04:12 +0000

    Add new runs

--- src/static/activities.js
+++ src/static/activities.js
@@ -4436,6 +4436,20 @@ const activities = [
     "average_heartrate": 148.0,
     "average_speed": 2.7056197007490512,
     "streak": 1
+  },
+  {
+    "run_id": 1632228195000,
+    "name": "run from gpx",
+    "distance": 8125.903486275127,
+    "moving_time": "0:48:31",
+    "type": "Run",
+    "start_date": "2021-09-21 12:43:15",
+    "start_date_local": "2021-09-21 20:43:15",
+    "location_country": "\u4e03\u4e2d\u521d\u4e2d\u9644\u5c0f, \u987a\u9526\u8def, \u6842\u6eaa\u8857\u9053, \u9ad8\u65b0\u5357\u533a, \u6b66\u4faf\u533a, \u6210\u90fd\u5e02, \u56db\u5ddd\u7701, 610041 CHENGDU, \u4e2d\u56fd",
+    "summary_polyline": "wlxyDcbgzRfCtAXlBxEjB~Bi@nANrRuArBf@dBuAxD}@rCeCrC_@~D\\|@r@d@vAFnHz@vCjCxDvAfApB\\rAmHaE}BB`@dBnAjD|A|GjArFBzD_AdO}GfGiAxK`@lNxDxE`@lFa@rG{BmFnBiIh@cI_AsIoCcJc@yGdAkOpGgH|AoF]yHcBmDoCE[^OzDtBgAnHe@Z_FaCaCsDi@_CRqCc@gEu@yAcAa@}GAkC~BwGnCsCe@eD`AiJF}Fn@kAgA_AOw@iA",
+    "average_heartrate": 151.0,
+    "average_speed": 2.791447436027182,
+    "streak": 1
   }
 ];
 

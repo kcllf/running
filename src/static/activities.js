ee61839ae924a9a244bd0aec6775b30ab612ca5c
kcllf <kcllf@qq.com>
2021-09-26 01:03:44 +0000

    Add new runs

--- src/static/activities.js
+++ src/static/activities.js
@@ -4464,6 +4464,20 @@ const activities = [
     "average_heartrate": 153.0,
     "average_speed": 2.6421361108192194,
     "streak": 1
+  },
+  {
+    "run_id": 1632573284000,
+    "name": "run from gpx",
+    "distance": 10018.357901351606,
+    "moving_time": "1:02:32",
+    "type": "Run",
+    "start_date": "2021-09-25 12:34:44",
+    "start_date_local": "2021-09-25 20:34:44",
+    "location_country": "\u4e03\u4e2d\u521d\u4e2d\u9644\u5c0f, \u987a\u9526\u8def, \u6842\u6eaa\u8857\u9053, \u9ad8\u65b0\u5357\u533a, \u6b66\u4faf\u533a, \u6210\u90fd\u5e02, \u56db\u5ddd\u7701, 610041 CHENGDU, \u4e2d\u56fd",
+    "summary_polyline": "{lxyDibgzRpBr@z@lC|EjBdCi@hALjQsAtC\\~GmCzA}ApCcAfEFlAl@|@tA`@`GMdBl@tBrBtDpBvAnBb@`@g@z@kGeEuBBl@tC|AhABf@t@vJtAvHq@vPaHfGqAjHDvPpEhEThJ}@tEmBgGdCuIj@mH}@qHcC{Jo@_J|AaTlIyGAoJ{BmDuBg@aC@uDm@qFeC}CuDuB_JYiDdDaExBgD|@wQjAiGoA}CgEyBcN}AqEwGyEuN{CfNlCxGzDfAfBj@dChB~MjCrDvGjBtMw@",
+    "average_heartrate": 151.0,
+    "average_speed": 2.670138033409277,
+    "streak": 1
   }
 ];
 
